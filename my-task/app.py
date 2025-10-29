d61f7e2628b561dada33b351b21ee476300008af
ROMLIANY <lianirom30@gmail.com>
2025-10-29 16:13:57 +0200

    app.py1

--- my-task/app.py
+++ my-task/app.py
@@ -1,11 +1,20 @@
 import requests
 import xml.etree.ElementTree as ET
 
+# url of ynet
 url = "https://www.ynet.co.il/Integration/StoryRss2.xml"
-response = requests.get(url)
 
+# info from web
+response = requests.get(url)
+response.raise_for_status()  # test errors
+
+# replace form
 root = ET.fromstring(response.content)
 
-for item in root.findall(".//item"):
-    title = item.find("title").text
-    print(title)
+items = root.findall(".//item")
+
+# print the title
+print(" כותרות אחרונות מ-Ynet:\n")
+for i, item in enumerate(items, start=1):
+    title = item.find("title").text.strip()
+    print(f"{i}. {title}")
